e9e13d83e482e2bece6ecb51ed396d8b7496f533
Emanuel Messias Vieira De Araújo <emanuelmvieira10@gmail.com>
2026-06-27 13:01:44 -0300

    Resolve merge conflicts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
+
 export const supabase = createClient(
   'https://dxkzktkeagpddmpcyooe.supabase.co',
-  'sb_publishable_-s5pTQGGUaqU11Zbki6iGw_3U3zRsur'
+  'sb_publishable_yx-UtDC0MIeiT5vITXk0Cw_oxTpa37x'
 )
+
+export const isSupabaseConfigured = () => true
